0ec4cf3750f63ec88eac166c74667d06fb6ace62
ArcaneArrow <emory@cooley.net>
2025-09-26 15:50:57 -0500

    Fixed Square Root

--- calc.py
+++ calc.py
@@ -5,22 +5,20 @@ import math
 def calculate(current_value, num, clear, sqrt):
     if clear:
         return ''  # Clear the current value if clear button is pressed
+    elif sqrt:
+        if sqrt == 'sqrt':
+            try:
+                return str(math.sqrt(eval(current_value)))
+            except:
+                return 'Error'
     elif num:
         if num == '=':
-            if sqrt == 'sqrt':
-                # Calculate result if '=' is pressed
-                try:
-                    # Safely evaluate the current expression
-                    return str(math.sqrt(eval(current_value)))
-                except:
-                    return 'Error'
-            else:
-                # Calculate result if '=' is pressed
-                try:
-                    # Safely evaluate the current expression
-                    return str(eval(current_value))
-                except:
-                    return 'Error'
+            # Calculate result if '=' is pressed
+            try:
+                # Safely evaluate the current expression
+                return str(eval(current_value))
+            except:
+                return 'Error'
         else:
             # Append the pressed button value to the current value
             return current_value + num
